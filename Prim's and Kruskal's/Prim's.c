cdc430b0d51367210c2121056f25902d51ded3e8
Nikhil M <nikhilm.cs24@bmsce.ac.in>
2026-04-21 09:54:21 +0530

    Update Prim's.c

--- Prim's and Kruskal's/Prim's.c
+++ Prim's and Kruskal's/Prim's.c
@@ -1,57 +1,208 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
+#include <time.h>
 
-#define V 5
+#define MAX 50001
 
-int minKey(int key[], int mstSet[]) {
-    int min = INT_MAX, min_index;
+typedef struct Edge {
+    int dest;
+    int weight;
+    struct Edge* next;
+} Edge;
 
-    for (int v = 0; v < V; v++)
-        if (mstSet[v] == 0 && key[v] < min)
-            min = key[v], min_index = v;
+typedef struct {
+    Edge* head[MAX];
+    int V;
+} Graph;
 
-    return min_index;
+typedef struct {
+    int v;
+    int key;
+} HeapNode;
+
+typedef struct {
+    int size;
+    int capacity;
+    int *pos;
+    HeapNode **array;
+} MinHeap;
+
+Edge* newEdge(int dest, int weight) {
+    Edge* e = (Edge*)malloc(sizeof(Edge));
+    e->dest = dest;
+    e->weight = weight;
+    e->next = NULL;
+    return e;
 }
 
-void primMST(int graph[V][V]) {
-    int parent[V];
-    int key[V];
-    int mstSet[V];
+Graph* createGraph(int V) {
+    Graph* graph = (Graph*)malloc(sizeof(Graph));
+    graph->V = V;
+    for (int i = 0; i < V; i++)
+        graph->head[i] = NULL;
+    return graph;
+}
 
-    for (int i = 0; i < V; i++) {
-        key[i] = INT_MAX;
-        mstSet[i] = 0;
+void addEdge(Graph* graph, int src, int dest, int weight) {
+    Edge* e = newEdge(dest, weight);
+    e->next = graph->head[src];
+    graph->head[src] = e;
+
+    e = newEdge(src, weight);
+    e->next = graph->head[dest];
+    graph->head[dest] = e;
+}
+
+HeapNode* newHeapNode(int v, int key) {
+    HeapNode* node = (HeapNode*)malloc(sizeof(HeapNode));
+    node->v = v;
+    node->key = key;
+    return node;
+}
+
+MinHeap* createMinHeap(int capacity) {
+    MinHeap* heap = (MinHeap*)malloc(sizeof(MinHeap));
+    heap->pos = (int*)malloc(capacity * sizeof(int));
+    heap->size = 0;
+    heap->capacity = capacity;
+    heap->array = (HeapNode**)malloc(capacity * sizeof(HeapNode*));
+    return heap;
+}
+
+void swapHeapNode(HeapNode** a, HeapNode** b) {
+    HeapNode* t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void minHeapify(MinHeap* heap, int idx) {
+    int smallest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+
+    if (left < heap->size &&
+        heap->array[left]->key < heap->array[smallest]->key)
+        smallest = left;
+
+    if (right < heap->size &&
+        heap->array[right]->key < heap->array[smallest]->key)
+        smallest = right;
+
+    if (smallest != idx) {
+        HeapNode *smallestNode = heap->array[smallest];
+        HeapNode *idxNode = heap->array[idx];
+
+        heap->pos[smallestNode->v] = idx;
+        heap->pos[idxNode->v] = smallest;
+
+        swapHeapNode(&heap->array[smallest], &heap->array[idx]);
+        minHeapify(heap, smallest);
+    }
+}
+
+HeapNode* extractMin(MinHeap* heap) {
+    if (heap->size == 0) return NULL;
+
+    HeapNode* root = heap->array[0];
+    HeapNode* lastNode = heap->array[heap->size - 1];
+
+    heap->array[0] = lastNode;
+
+    heap->pos[root->v] = heap->size - 1;
+    heap->pos[lastNode->v] = 0;
+
+    heap->size--;
+    minHeapify(heap, 0);
+
+    return root;
+}
+
+void decreaseKey(MinHeap* heap, int v, int key) {
+    int i = heap->pos[v];
+    heap->array[i]->key = key;
+
+    while (i && heap->array[i]->key < heap->array[(i - 1) / 2]->key) {
+        heap->pos[heap->array[i]->v] = (i - 1) / 2;
+        heap->pos[heap->array[(i - 1) / 2]->v] = i;
+
+        swapHeapNode(&heap->array[i],
+                     &heap->array[(i - 1) / 2]);
+
+        i = (i - 1) / 2;
+    }
+}
+
+int isInMinHeap(MinHeap *heap, int v) {
+    return heap->pos[v] < heap->size;
+}
+
+int primMST(Graph* graph) {
+    int V = graph->V;
+    int key[V];
+    MinHeap* heap = createMinHeap(V);
+
+    for (int v = 0; v < V; v++) {
+        key[v] = INT_MAX;
+        heap->array[v] = newHeapNode(v, key[v]);
+        heap->pos[v] = v;
     }
 
     key[0] = 0;
-    parent[0] = -1;
+    decreaseKey(heap, 0, key[0]);
+    heap->size = V;
 
-    for (int count = 0; count < V - 1; count++) {
-        int u = minKey(key, mstSet);
-        mstSet[u] = 1;
+    int total_weight = 0;
 
-        for (int v = 0; v < V; v++) {
-            if (graph[u][v] && mstSet[v] == 0 && graph[u][v] < key[v]) {
-                parent[v] = u;
-                key[v] = graph[u][v];
+    while (heap->size > 0) {
+        HeapNode* minNode = extractMin(heap);
+        int u = minNode->v;
+        total_weight += minNode->key;
+
+        Edge* crawl = graph->head[u];
+        while (crawl != NULL) {
+            int v = crawl->dest;
+
+            if (isInMinHeap(heap, v) &&
+                crawl->weight < key[v]) {
+                key[v] = crawl->weight;
+                decreaseKey(heap, v, key[v]);
             }
+            crawl = crawl->next;
         }
     }
 
-    printf("Edge \tWeight\n");
-    for (int i = 1; i < V; i++)
-        printf("%d - %d \t%d\n", parent[i], i, graph[i][parent[i]]);
+    return total_weight;
+}
+
+Graph* generateGraph(int V) {
+    Graph* graph = createGraph(V);
+
+    for (int i = 0; i < V; i++) {
+        for (int j = i + 1; j < V && j < i + 5; j++) {
+            int weight = rand() % 100 + 1;
+            addEdge(graph, i, j, weight);
+        }
+    }
+    return graph;
 }
 
 int main() {
-    int graph[V][V] = {
-        {0, 2, 0, 6, 0},
-        {2, 0, 3, 8, 5},
-        {0, 3, 0, 0, 7},
-        {6, 8, 0, 0, 9},
-        {0, 5, 7, 9, 0}
-    };
+    srand(time(NULL));
 
-    primMST(graph);
+    printf("Vertices\tTime (seconds)\n");
+
+    for (int size = 10000; size <= 50000; size += 10000) {
+        Graph* graph = generateGraph(size);
+
+        clock_t start = clock();
+        primMST(graph);
+        clock_t end = clock();
+
+        double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+        printf("%d\t\t%f\n", size, time_taken);
+    }
+
     return 0;
 }
